61bcd04841506f38f3e7c3b242739c56773ad7f9
Sulaimon Quadri Dolapo <dontezzy01@gmail.com>
2022-09-09 06:33:14 -0700

    Task4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 /**
  * Main: Entry point
- * 
+ * Description: Puts text 
  * Return: always zero
  */
 int main(void)
+	
 {
 puts("\"Programming is like building a multilingual puzzle");
 return 0;
