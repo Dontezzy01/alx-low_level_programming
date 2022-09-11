701c910f2b80418a297619b262e4110dbb3bf26f
Sulaimon Quadri Dolapo <dontezzy01@gmail.com>
2022-09-11 15:32:53 -0700

    Task 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main- Program that print single digit
+ * Return: 0
+ */
+int main(void)
+{
+int n;
+while (n < 10)
+{
+printf("%d", n);
+n += 1;
+}
+printf("\n");
+return (0);
+}
