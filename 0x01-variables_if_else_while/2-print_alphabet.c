766bfc6bce6da8e8fc6b99c15eaa82f77329acb5
Sulaimon Quadri Dolapo <dontezzy01@gmail.com>
2022-09-11 12:26:56 -0700

    Task2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <ctype.h>
+/**
+ * main- To print alphabet in lowercase
+ * Return: 0
+ */
+int main(void)
+{
+int l = 'a';
+while (l <= 'z')
+{
+putchar(l);
+l += 1;
+}
+putchar('\n');
+return (0);
+}
