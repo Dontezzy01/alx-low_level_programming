9c7d2f9e15062f96041afe3cc7cca8e4edf5a33f
Sulaimon Quadri Dolapo <dontezzy01@gmail.com>
2022-09-12 17:12:52 -0700

    Task 7

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main- Program that print single digit
+ * Return: 0
+ */
+int maim(void)
+{
+int d;
+for (d ='0'; d <= '9'; d++)
+putchar(d);
+putchar('\n');
+return (0);
+}
