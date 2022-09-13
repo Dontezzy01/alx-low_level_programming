6e584b27c8f400517d60cca26747e9acf1de1281
Sulaimon Quadri Dolapo <dontezzy01@gmail.com>
2022-09-12 17:22:58 -0700

    Task 7

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -1,6 +1,4 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
 /**
  * main- Program to print low
  * Return: 0
@@ -8,7 +6,7 @@
 int main(void)
 {
 char low;
-for (low = 'z'; low >= 'a'; low)
+for (low = 'z'; low >= 'a'; low--)
 putchar(low);
 putchar('\n');
 return (0);
