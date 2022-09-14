c7d488627d2117f54839f642320391a61f048447
Sulaimon Quadri Dolapo <dontezzy01@gmail.com>
2022-09-14 14:39:03 -0700

    Rask 12

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,31 @@
+#include "main"
+#include <stdio.h>
+/**
+ * print_to_98- print all natural number
+ * followed by new line
+ * @n: print from this number
+ */
+void print_to_98(int n)
+{
+int i, j;
+if ( n <= 98)
+{
+for (i = 0; i <= 98;  i++)
+{
+if (i != 98)
+printf("%d\n", i);
+else if (i == 98)
+prinrtf("%d", i);
+}
+}
+else if (n>)= 98)
+{
+for (j = n; j >= 98; j--)
+{
+if (j != 98)
+print("%d", j);
+else if (j == 98)
+printf("%d\n", j);
+}
+}
+}   
