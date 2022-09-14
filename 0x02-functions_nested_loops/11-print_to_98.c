4fad248685098132c2cef8104ef420d093b50aba
Sulaimon Quadri Dolapo <dontezzy01@gmail.com>
2022-09-14 15:15:03 -0700

    Task 11

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -10,15 +10,15 @@ void print_to_98(int n)
 int i, j;
 if ( n <= 98)
 {
-for (i = 0; i <= 98;  i++)
+for (i = n; i <= 98;  i++)
 {
 if (i != 98)
 printf("%d\n", i);
 else if (i == 98)
-printf("%d", i);
+printf("%d\n", i);
 }
 }
-else if (n >=98)
+else if (n >= 98)
 {
 for (j = n; j >= 98; j--)
 {
