d63f2c1e33d1b35e3388d530e3d102889fbd7841
Sulaimon Quadri Dolapo <dontezzy01@gmail.com>
2022-09-14 06:58:17 -0700

    Task 6

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _abs- compute absolute number of an interger
+ * @n- the int to check
+ * Return: the abosulute value
+ */
+int _abs(int)
+{
+if (n >= 0)
+{
+return(n);
+}
+return(-n);
+}
