2c09d5e1e8ad6a68a9dd79488f32d14a285dde8b
Sulaimon Quadri Dolapo <dontezzy01@gmail.com>
2022-09-14 03:46:13 -0700

    Task 2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * print_alphabet_x10- Print letters 10 times
+ * Return: 0
+ */
+void print_alphabet_x10.c
+{
+int i;
+char c;
+for (i = 0; i <= 9; i++)
+{
+for (c = 'a'; C <= 'z'; c++)
+{
+_putchar(c);
+}
+_putchar('\n');
+}
+}
