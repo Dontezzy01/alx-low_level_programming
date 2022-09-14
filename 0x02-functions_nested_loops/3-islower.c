b1e9c89f2f6d8f15f1252a8662ffba159c7b6551
Sulaimon Quadri Dolapo <dontezzy01@gmail.com>
2022-09-14 04:10:26 -0700

    Task 2

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,8 +1,9 @@
 #include "main.h"
 /**
  * _islower- check for lowercase
- * Return: 1 if success
+ * Return: 1 if c is lowercase, 0 otherwise
  */
+
 int _islower(int c)
 {
 return (c >= 'a' && c <= 'z');
