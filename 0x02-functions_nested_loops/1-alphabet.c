722bc92aec211a3e325bcaa820abbef4b1e4bb55
Sulaimon Quadri Dolapo <dontezzy01@gmail.com>
2022-09-14 01:56:51 -0700

    Task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * main- Alphabet_x10
+ * Return: 0
+ */
+void print_alphabet_x10(void);
+{
+char c;
+int i;
+for(i = 0; i < 9; i++)
+{ 
+for(c = 'a'; c < 'z'; c++)
+{
+_putchar(c);
+}
+_putchar('\n');
+}
+}
