3670656648a7cb9d8debbb528cd60895bdff9544
Sulaimon Quadri Dolapo <dontezzy01@gmail.com>
2022-09-14 04:47:01 -0700

    TASk 4

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * _isalpha- checks for alphatbet
+ * @c: chracter to be checked
+ * Return: 1 if c is character,0 ottherwise
+ */
+int _isalpha(int c)
+{
+return (( c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'));
+}
