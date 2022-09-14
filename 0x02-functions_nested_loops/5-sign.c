48038bb625069b7d214a8117d57a87431e479130
Sulaimon Quadri Dolapo <dontezzy01@gmail.com>
2022-09-14 06:01:54 -0700

    Task 5

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -3,8 +3,8 @@
  * print_sign- print the sign of a number
  * @c: the int to check
  * Return: 1 and print + if the n is greater than 0
- * 0 and print if n is 0
- * -l and print if n is less than 0
+ * 0 and print zeri if n is 0
+ * -l and print - if n is less than 0
  */
 int print_sign(int n)
 {
@@ -22,5 +22,5 @@ else if (n < 0)
 {
 _putchar('-');
 }
-return(-1);
+return (-1);
 }
