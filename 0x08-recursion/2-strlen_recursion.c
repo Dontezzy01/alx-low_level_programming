d949372a5b3c2b9bb026e3fca9245ea35bf96778
Sulaimon Quadri Dolapo <dontezzy01@gmail.com>
2022-09-28 10:50:22 -0700

    .

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * _print_rev_recursion - prints a string in reverse
+ * @s: string to print
+ * Return: void
+ */
+int _strlen_recursion(char *s)
+{
+	if (*s == '\0')
+		return (0);
+	return (1 + _strlen_recursion(s + 1));
+}
